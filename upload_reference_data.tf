d7b5ba38973be3aab763b6b3ba506aa251501ba8
Sven Erik Knop <seknop@gmail.com>
2023-11-10 18:32:55 +0000

    Upload LOCATIONS data with new Producer application

---
+++ upload_reference_data.tf
@@ -0,0 +1,13 @@
+variable "ukrail_loccations" {
+  default = "ukrail_locations.csv"
+}
+
+resource "null_resource" "ukrail_locations_upload" {
+  provisioner "local-exec" {
+    command = "java -cp CSVFileAvroUploader-1.0.0.jar io.confluent.bootcamp.CSVFileAvroUploader -c ${var.ccloud-properties} -f ${var.ukrail_loccations} --topic ${confluent_kafka_topic.LOCATIONS.topic_name} --key-field location_id"
+  }
+
+  depends_on = [
+    confluent_kafka_topic.LOCATIONS
+  ]
+}
